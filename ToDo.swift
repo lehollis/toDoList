4395218d68dbb98c4b79dc11ac849fce37f92e22
Lauren Hollis <leholllis@gmail.com>
2019-07-15 11:47:51 -0500

    Completed iteration 0

---
+++ ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoList
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Lauren Hollis. All rights reserved.
+//
+import UIKit
+
+class ToDo{
+    var name = " "
+    var important = false
+}
